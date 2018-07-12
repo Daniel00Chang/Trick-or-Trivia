efe959291ecaef52ca70a4952a269c94b00ccaaf
VADER\krist <kristianbrasel@gmail.com>
2018-07-11 20:00:25 -0700

    formatting, expanding for scalability.

--- TrickorTrivia_v2.0.py
+++ TrickorTrivia_v2.0.py
@@ -37,18 +37,30 @@ from tkinter import *  # import tkinter library for GUI, from shortens function 
 import time  # used for sleep commands
 import sys  # if you used an exit option, you would need this
 import pygame  # used to play audio
-from pygame.locals import *  # not sure what this was used for
 from random import randint  # a random number is used to select a random quiz
-##GPIO.setwarnings(False)       # I don't think this is necessary if GPIO is cleaned upon exit/crash
+# GPIO.setwarnings(False)       # I don't think this is necessary if GPIO is cleaned upon exit/crash
 
-import quizs.defaultQuiz as defaultQuiz
-import quizs.halloween as halloween
+# import quizs.defaultQuiz as defaultQuiz
+# import quizs.halloween as halloween
 
-theme = halloween.halloween()
+# from quizs import *
+import quizs as test
+
+theme = test.defaultQuiz.defaultQuiz()
+
+
+
+# theme = defaultQuiz.defaultQuiz()
 quiz = theme.getQuiz()
 
 
+def importQuizList():
+    import quizs.defaultQuiz as defaultQuiz
+    import quizs.halloween as halloween
+
+
 class App(Frame):
+    # import quizs as test
 
     # init is run on object creation
     def __init__(self, master):
@@ -58,44 +70,103 @@ class App(Frame):
         self.playSound(game_open)  # This will play as soon as the app is started
 
     def populateQuiz(self):
-        theme = halloween.halloween()
+        theme = test.halloween.halloween()
         quiz = theme.getQuiz()
 
     # This re-prints the header, it's called before each question is printed
     def header(self):
-        self.label_1 = Label(root, text="Welcome to " + theme.getQuizName(), font=(theme.getFont(), 36),
-                             bg=theme.getBGColor(), fg=theme.getTitleColor())
-        self.label_1.grid(columnspan=6, padx=(100, 10))
-        self.label_2 = Label(root, text="Answer the question for candy!", font=(theme.getFont(), 28),
-                             bg=theme.getBGColor(), fg=theme.getSecondaryColor())
-        self.label_2.grid(columnspan=6, pady=5, padx=(100, 10))
+        self.label_1 = Label(root,
+                             text="Welcome to " + theme.getQuizName(),
+                             font=(theme.getFont(), 36),
+                             bg=theme.getBGColor(),
+                             fg=theme.getTitleColor())
+        self.label_1.grid(columnspan=6,
+                          padx=(100, 10))
 
-    # I had to add this label, otherwise it would crash when I try to delete label_7 in selectQuiz() on my first run
+        self.label_2 = Label(root,
+                             text="Answer the question for candy!",
+                             font=(theme.getFont(), 28),
+                             bg=theme.getBGColor(),
+                             fg=theme.getSecondaryColor())
+        self.label_2.grid(columnspan=6,
+                          pady=5,
+                          padx=(100, 10))
+
+    # I had to add this label, otherwise it would crash when I try to delete label_7 in selectQuestion() on my first run
     def prime(self):
-        self.label_7 = Label(root, text="I serve candy.", font=(theme.getFont(), 32), bg=theme.getBGColor(),
+        self.label_7 = Label(root,
+                             text="I serve candy.",
+                             font=(theme.getFont(), 32),
+                             bg=theme.getBGColor(),
                              fg=theme.getFGColor())
-        self.label_7.grid(columnspan=6, pady=5, padx=(100, 10))
+        self.label_7.grid(columnspan=6,
+                          pady=5,
+                          padx=(100, 10))
 
     # Prints select difficulty buttons
     def difficulty(self):
-        self.label_8 = Label(root, text="Please choose a difficulty.", font=(theme.getFont(), 32),
-                             bg=theme.getBGColor(), fg=theme.getFGColor())
-        self.label_8.grid(columnspan=6, pady=5, padx=(100, 10))
+        self.label_8 = Label(root,
+                             text="Please choose a difficulty.",
+                             font=(theme.getFont(), 32),
+                             bg=theme.getBGColor(),
+                             fg=theme.getFGColor())
+        self.label_8.grid(columnspan=6,
+                          pady=5,
+                          padx=(100, 10))
 
-        self.button_8 = Button(root, text="Easy", font=(theme.getFont(), 36),
-                               command=lambda: self.selectQuiz(0))  # lambda allows you to pass an argument
-        self.button_8.grid(row=5, column=1, padx=(100, 10))
-        self.button_9 = Button(root, text="Medium", font=(theme.getFont(), 36), command=lambda: self.selectQuiz(1))
-        self.button_9.grid(row=5, column=3, padx=(100, 10))
-        self.button_10 = Button(root, text="Hard", font=(theme.getFont(), 36), command=lambda: self.selectQuiz(2))
-        self.button_10.grid(row=5, column=5, padx=(100, 10))
+        self.button_8 = Button(root,
+                               text="Easy",
+                               font=(theme.getFont(), 36),
+                               command=lambda: self.selectQuestion(0))  # lambda allows you to pass an argument
+        self.button_8.grid(row=5,
+                           column=1,
+                           padx=(100, 10))
 
-        self.label_11 = Label(root, text="Prizes: Easy = 1 candy, Medium = 2 candies, Hard = 3 candies",
-                              font=(theme.getFont(), 16), bg=theme.getBGColor(), fg=theme.getFGColor())
-        self.label_11.grid(columnspan=6, pady=5, padx=(100, 10))
+        self.button_9 = Button(root,
+                               text="Medium",
+                               font=(theme.getFont(), 36),
+                               command=lambda: self.selectQuestion(1))
+        self.button_9.grid(row=5,
+                           column=3,
+                           padx=(100, 10))
+
+        self.button_10 = Button(root,
+                                text="Hard",
+                                font=(theme.getFont(), 36),
+                                command=lambda: self.selectQuestion(2))
+        self.button_10.grid(row=5,
+                            column=5,
+                            padx=(100, 10))
+
+        self.label_11 = Label(root,
+                              text="Prizes: Easy = 1 candy, Medium = 2 candies, Hard = 3 candies",
+                              font=(theme.getFont(), 16),
+                              bg=theme.getBGColor(),
+                              fg=theme.getFGColor())
+        self.label_11.grid(columnspan=6,
+                           pady=5,
+                           padx=(100, 10))
+
+        self.button_theme = Button(root,
+                                   text="Theme",
+                                   font=(theme.getFont(), 16),
+                                   command=lambda: self.selectTheme())
+        self.button_theme.grid(row=1,
+                               column=6,
+                               padx=(100,10))
+
+
+    def selectTheme(self):
+        self.label_7.destroy()  # destroys the label from correct() wrong()
+        self.label_8.destroy()  # destroys all the labels and buttons from difficulty()
+        self.button_8.destroy()
+        self.button_9.destroy()
+        self.button_10.destroy()
+        self.label_11.destroy()
+
 
     # Randomly selects a question in the correct difficulty
-    def selectQuiz(self, difficulty):
+    def selectQuestion(self, difficulty):
 
         self.label_7.destroy()  # destroys the label from correct() wrong()
         self.label_8.destroy()  # destroys all the labels and buttons from difficulty()
@@ -104,11 +175,11 @@ class App(Frame):
         self.button_10.destroy()
         self.label_11.destroy()
 
-        quantity = (int(
-            len(quiz[difficulty])) - 1)  # gets the number of questions in selected difficulty, cast int to avoid errors
+
+
+        quantity = (int(len(quiz[difficulty])) - 1)  # gets the number of questions in selected difficulty, cast int to avoid errors
         randomSelection = int(randint(0, quantity))  # randomly selects a number in a valid range
-        print(
-            randomSelection)  # prints the number selected to the console. I used this for error checking, but it isnt necessary for the game.
+        print(randomSelection)  # prints the number selected to the console. I used this for error checking, but it isnt necessary for the game.
 
         # pass values to be printed to screen. [difficulty][selection][data]
         self.quizMe(quiz[difficulty][randomSelection][0],  # question
@@ -122,8 +193,7 @@ class App(Frame):
     # This method accepts all quiz info and prints it to the screen
     def quizMe(self, question, choice1, choice2, choice3, choice4, correctChoice, difficulty):
 
-        result = [self.wrong, self.wrong, self.wrong, self.wrong,
-                  self.wrong]  # array to hold method names for button commands. Prime array by filling with wrong
+        result = [self.wrong, self.wrong, self.wrong, self.wrong, self.wrong]  # array to hold method names for button commands. Prime array by filling with wrong
         result[correctChoice] = self.correct  # assigns correct answer in correct possition
 
         choice = [choice1, choice2, choice3, choice4]  # array for font sizing
@@ -132,6 +202,7 @@ class App(Frame):
         for x in range(0, 4):  # cycles through all the choices to find the longest string.
             if len(choice[x]) > 8:
                 fontSizeB = 24
+
         # check to see what font size is necessary for the question. I'm looking for a more dynamic solution.
         fontSizeL = 32
         if len(question) > 30:
@@ -142,37 +213,69 @@ class App(Frame):
                     fontSizeL = 22
 
         # question
-        self.label_3 = Label(root, text=question, font=(theme.getFont(), fontSizeL), bg=theme.getBGColor(),
+        self.label_3 = Label(root,
+                             text=question,
+                             font=(theme.getFont(), fontSizeL),
+                             bg=theme.getBGColor(),
                              fg=theme.getFGColor())
-        self.label_3.grid(columnspan=6, pady=5, padx=(100, 10))
+
+        self.label_3.grid(columnspan=6,
+                          pady=5,
+                          padx=(100, 10))
 
         # choice1
-        self.button_1 = Button(root, text=choice1, font=(theme.getFont(), fontSizeB),
+        self.button_1 = Button(root,
+                               text=choice1,
+                               font=(theme.getFont(), fontSizeB),
                                command=lambda: result[1](difficulty))  # lambda allows you to pass an argument
-        self.button_1.grid(row=4, column=1, pady=5, padx=(100, 10))
+        self.button_1.grid(row=4,
+                           column=1,
+                           pady=5,
+                           padx=(100, 10))
 
         # choice2
-        self.button_2 = Button(root, text=choice2, font=(theme.getFont(), fontSizeB),
+        self.button_2 = Button(root,
+                               text=choice2,
+                               font=(theme.getFont(), fontSizeB),
                                command=lambda: result[2](difficulty))
-        self.button_2.grid(row=4, column=3, sticky=W, padx=(100, 10))
+        self.button_2.grid(row=4,
+                           column=3,
+                           sticky=W,
+                           padx=(100, 10))
 
         # choice3
-        self.button_3 = Button(root, text=choice3, font=(theme.getFont(), fontSizeB),
+        self.button_3 = Button(root,
+                               text=choice3,
+                               font=(theme.getFont(), fontSizeB),
                                command=lambda: result[3](difficulty))
-        self.button_3.grid(row=5, column=1, pady=5, padx=(100, 10))
+        self.button_3.grid(row=5,
+                           column=1,
+                           pady=5,
+                           padx=(100, 10))
 
         # choice4
-        self.button_4 = Button(root, text=choice4, font=(theme.getFont(), fontSizeB),
+        self.button_4 = Button(root,
+                               text=choice4,
+                               font=(theme.getFont(), fontSizeB),
                                command=lambda: result[4](difficulty))
-        self.button_4.grid(row=5, column=3, sticky=W, padx=(100, 10))
+        self.button_4.grid(row=5,
+                           column=3,
+                           sticky=W,
+                           padx=(100, 10))
 
     # Executes all necessary functions for a correct answer
     def correct(self, difficulty):
         self.clearPage()  # Destroys all buttons and lables on screen before displaying results
 
-        self.label_7 = Label(root, text="CORRECT!!!!", font=(theme.getFont(), 32), bg=theme.getBGColor(),
+        self.label_7 = Label(root,
+                             text="CORRECT!!!!",
+                             font=(theme.getFont(), 32),
+                             bg=theme.getBGColor(),
                              fg=theme.getFGColor())
-        self.label_7.grid(columnspan=6, pady=5, padx=(100, 10))
+
+        self.label_7.grid(columnspan=6,
+                          pady=5,
+                          padx=(100, 10))
 
         self.playSound(correct_audio_path)  # Send the filepath of the audio to play for correct answer
         self.pushCandy((difficulty + 1))  # dispenses appropriate number of candies for difficulty
@@ -182,9 +285,15 @@ class App(Frame):
     def wrong(self, difficulty):
         self.clearPage()  # Destroys all buttons and lables on screen before displaying results
 
-        self.label_7 = Label(root, text="Wrong, but thanks for playing.", font=(theme.getFont(), 32),
-                             bg=theme.getBGColor(), fg=theme.getFGColor())
-        self.label_7.grid(columnspan=6, pady=5, padx=(100, 10))
+        self.label_7 = Label(root,
+                             text="Wrong, but thanks for playing.",
+                             font=(theme.getFont(), 32),
+                             bg=theme.getBGColor(),
+                             fg=theme.getFGColor())
+
+        self.label_7.grid(columnspan=6,
+                          pady=5,
+                          padx=(100, 10))
 
         self.playSound(incorrect_audio_path)  # Send the filepath of the audio to play for correct answer
         self.pushCandy(1)  # dispenses 1 candy for incorrect guess
@@ -199,8 +308,7 @@ class App(Frame):
         p.start(2.5)  # duty cycle, nutral position
 
         while candies > 0:
-            p.ChangeDutyCycle(
-                12)  # Pull plunger. This number may need to be altered depending on your servo and orientation.
+            p.ChangeDutyCycle(12)  # Pull plunger. This number may need to be altered depending on your servo and orientation.
             time.sleep(.4)  # Gives the servo time to transition
             p.ChangeDutyCycle(2.5)  # Push plunger. This number may also need to be altered.
             time.sleep(.4)
@@ -208,8 +316,7 @@ class App(Frame):
 
         p.stop()  # stops servo control
         GPIO.cleanup()  # cleans the GPIO settings. Without this the servo acts irratically between uses.
-        print(
-            "candy")  # When testing on desktop, comment all other lines in this method and leave the print to avoid error.
+        print("candy")  # When testing on desktop, comment all other lines in this method and leave the print to avoid error.
 
     # play sound from whatever file is at parameter dirrectory. I used this to play a sound effect at multiple points.
     def playSound(self, fileName):
@@ -230,20 +337,22 @@ class App(Frame):
 root = Tk()
 root.title("Trick or Trivia")  # adds a name to the titlebar of the application
 root.overrideredirect(True)  # This takes up the whole screen and removes x to exit
-root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(),
-                                   root.winfo_screenheight()))  # when I was testing on desktop I replaced {0}x{1}+0+0 with the resolution of my touchscreen
+root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(),root.winfo_screenheight()))  # when I was testing on desktop I replaced {0}x{1}+0+0 with the resolution of my touchscreen
 root.focus_set()  # <-- move focus to this widget
+
 if (onPi == True):
-    root.configure(background='black',
-                   cursor="none")  # removes cursor for a cleaner look, relies on touchscreen. Remove cursor="none" if you need a mouse.
+    root.configure(background='black',cursor="none")  # removes cursor for a cleaner look, relies on touchscreen. Remove cursor="none" if you need a mouse.
 else:
     root.configure(background='black')
+
 app = App(master=root)  # object instantiation
 # app.populateQuiz()
 app.header()
 app.prime()
 app.difficulty()
-Button(app, text="X", command=root.destroy, bg="red").pack()
-Button(app, text="Theme", bg="white").pack()
+Button(app,
+       text="X",
+       command=root.destroy,
+       bg="red").pack()
 
 app.mainloop()  # This is necessary for event programming in tkinter
---
+++ quizs/__init__.py
@@ -0,0 +1,9 @@
+from . import *
+test = ["defaultQuiz", "halloween"]
+
+def __init__():
+    from . import defaultQuiz
+    from . import halloween
+
+
+__init__()
